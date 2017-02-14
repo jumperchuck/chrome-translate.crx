3d4fd7c70b362e5479ddc12d17fc0b6efba09e9e
jumperChuck <ck_125s@163.com>
2017-02-14 18:08:10 +0800

    change

---
+++ translate/js/test.js
@@ -0,0 +1,45 @@
+/**
+ * Created by Administrator on 2017/2/13.
+ */
+window.onload = function(){
+    var inp = document.getElementById('inp');
+    var btn = document.getElementById('btn');
+    var result = document.getElementById('result');
+    var bg = document.getElementById('bg');
+    var ball = document.getElementById('ball');
+    var onOff = true;
+    bg.onclick = function(){
+        if(onOff){
+            this.style.backgroundColor = '#f8001b';
+            ball.style.marginLeft = '15px';
+            onOff = false;
+        }else{
+            this.style.backgroundColor = '#7fa2c3';
+            ball.style.marginLeft = 0;
+            onOff = true;
+        }
+    }
+    btn.onclick = function(){
+        var txt = inp.value;
+        baiduFanyi(txt,function(data){
+            result.innerHTML = data;
+        });
+    }
+}
+function httpRequest(url,callback){
+    var xhr = new XMLHttpRequest();
+    xhr.open("get", url, true);
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4) {
+            callback(xhr.responseText);
+        }
+    }
+    xhr.send();
+}
+function baiduFanyi(text,callback) {
+    var url = 'https://fanyi.baidu.com/v2transapi?query=' + encodeURIComponent(text) + '&lang=auto';
+    httpRequest(url,function(r){
+        result = JSON.parse(r);
+        callback && callback(result.trans_result.data[0].dst);
+    });
+}
--- translate/js/translate.js
+++ translate/js/translate.js
@@ -1,18 +1,5 @@
-﻿var result = null;
+var result = null;
 var newDiv = document.createElement('div');
-var img = document.createElement('img');
-var sp1 = document.createElement('span');
-var sp2 = document.createElement('span');
-img.id = 'ck';
-img.alt = '帅哥';
-img.src = 'http://jumperchuck.github.io/img/me.jpg';
-img.style.marginRight = '5px';
-img.style.width = '40px';
-img.style.verticalAlign = 'middle';
-img.style.borderRadius = '50%';
-img.style.transition = 'transform 1s';
-sp1.innerHTML = '译:';
-sp1.style.color = '#999';
 newDiv.style.padding = '2px 6px';
 newDiv.style.background = '#fff';
 newDiv.style.boxShadow = '0px 0px 5px #999';
@@ -20,8 +7,28 @@ newDiv.style.position = 'fixed';
 newDiv.style.zIndex = '1000';
 newDiv.style.fontSize = '14px';
 newDiv.style.borderRadius = '3px';
+newDiv.style.lineHeight = '40px';
+newDiv.style.textAlign = 'left';
+var img = document.createElement('img');
+img.id = 'ck';
+img.alt = '帅哥';
+img.src = 'https://jumperchuck.github.io/img/me.jpg';
+img.style.marginRight = '5px';
+img.style.width = '40px';
+img.style.verticalAlign = 'middle';
+img.style.borderRadius = '50%';
+img.style.transition = 'transform 1s';
+
+var span = document.createElement('span');
+span.innerHTML = '译:';
+span.style.color = '#999';
+
+var conText = document.createElement('span');
+conText.style.color = '#000';
+
 newDiv.appendChild(img);
-newDiv.appendChild(sp1);
+newDiv.appendChild(span);
+newDiv.appendChild(conText);
 newDiv.onmouseup = function(e){
     var e = e||event;
     e.cancelBubble = true;
@@ -37,11 +44,24 @@ document.onmouseup = function(e){
     var e = e || event;
     var text = window.getSelection();
     if( text != '' ){
-        baiduFanyi(text.toString());
-        sp2.innerHTML = '';
-        newDiv.style.left = e.clientX + 'px';
-        newDiv.style.top = e.clientY + 15 + 'px';
+        conText.innerHTML = '';
         document.body.appendChild(newDiv);
+        baiduFanyi(text.toString(),function(){
+            var l = e.clientX + newDiv.offsetWidth;
+            var t = e.clientY + newDiv.offsetHeight;
+            if(document.body.clientWidth > newDiv.offsetWidth && l > document.body.clientWidth){
+                l = e.clientX - (l - document.body.clientWidth);
+                newDiv.style.left = l + 'px';
+            }else {
+                newDiv.style.left = e.clientX + 'px';
+            }
+            if(document.body.clientHeight > newDiv.offsetHeight && t > document.body.clientHeight){
+                t = e.clientY - (t-document.body.clientHeight);
+                newDiv.style.top = t + 15 + 'px';
+            }else {
+                newDiv.style.top = e.clientY + 15 + 'px';
+            }
+        });
         onOff = true;
     }
     else{
@@ -59,14 +79,16 @@ function httpRequest(url,callback){
             callback(xhr.responseText);
         }
     }
+    xhr.onerror = function(){
+        callback(false);
+    }
     xhr.send();
 }
-function baiduFanyi(text) {
-    var url = 'http://fanyi.baidu.com/v2transapi?query=' + encodeURIComponent(text) + '&lang=auto';
+function baiduFanyi(text,callback) {
+    var url = 'https://fanyi.baidu.com/v2transapi?query=' + encodeURIComponent(text) + '&lang=auto';
     httpRequest(url,function(r){
         result = JSON.parse(r);
-        sp2.innerHTML = result.trans_result.data[0].dst;
-        newDiv.appendChild(sp2);
-        // newDiv.innerHTML = span + result.trans_result.data[0].dst;
+        conText.innerHTML = result.trans_result.data[0].dst;
+        callback && callback();
     });
 }
